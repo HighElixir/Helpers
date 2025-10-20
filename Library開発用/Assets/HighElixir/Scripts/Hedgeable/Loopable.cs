7e592ad280bd26703f333826f3de11e14c082855
HighElixir <HighElixir@users.noreply.github.com>
2025-10-20 23:07:05 +0900

    Loopableの変更破棄

--- Library開発用/Assets/HighElixir/Scripts/Hedgeable/Loopable.cs
+++ Library開発用/Assets/HighElixir/Scripts/Hedgeable/Loopable.cs
@@ -1,116 +1,92 @@
-﻿using System;
-using HighElixir.Implements;
+﻿using HighElixir.Implements;
+using System;
 
 namespace HighElixir.Hedgeable
 {
-    /// <summary>
-    /// 特定の範囲内で循環（ループ）する値を扱う構造体。
-    /// </summary>
-    public struct Loopable<T> : IHedgeable<T, Loopable<T>>
-        where T : struct, IComparable<T>, IEquatable<T>
+    public class LoopableInt : IHedgeable<int, LoopableInt>
     {
-        private T _value;
-        private T _minValue;
-        private T _maxValue;
-        private int _direction;
-        private Action<T, T> _onLoop;
+        private int _value;
+        private int _minValue = int.MinValue;
+        private int _maxValue = int.MaxValue;
 
+        // -1 ならば負方向、+1 ならば正方向、0 ならば変化なし
+        private int _direction = 0;
+        private Action<int, int> _onLoop;
         public int Direction
         {
             get => _direction;
             private set => _direction = (value == 0) ? 0 : value / Math.Abs(value);
         }
-
-        public T Value
+        public int Value
         {
             get => _value;
             set
             {
-                var oldValue = _value;
-                _value = LoopValue(value);
+                int oldValue = _value;
+                int rangeSize = _maxValue - _minValue + 1;
 
-                int diff = Compare(_value, oldValue);
+                if (rangeSize <= 0)
+                {
+                    _value = _minValue;
+                    return;
+                }
+
+                int mod = (value - _minValue) % rangeSize;
+                if (mod < 0) mod += rangeSize;
+
+                int newValue = _minValue + mod;
+
+                // Direction の更新
+                int diff = newValue - oldValue;
                 Direction = diff;
 
-                // ループ判定：範囲を超えて循環した時だけ通知
-                if (IsLooped(oldValue, value))
+                // 値の更新
+                _value = newValue;
+
+                // ループ発動条件：入力と出力の差が rangeSize を超えていたら
+                if (Math.Abs(value - oldValue) >= rangeSize)
                 {
                     _onLoop?.Invoke(oldValue, _value);
                 }
             }
         }
-
-        public T MinValue => _minValue;
-        public T MaxValue => _maxValue;
-
-        public Loopable(T minValue, T maxValue, T initialValue)
+        public int MinValue => _minValue;
+        public int MaxValue => _maxValue;
+        public LoopableInt(int initialValue = 0)
+        {
+            Value = initialValue;
+        }
+        public LoopableInt(int minValue, int maxValue, int initialValue = 0)
         {
             _minValue = minValue;
             _maxValue = maxValue;
-            _direction = 0;
-            _onLoop = null;
-            _value = LoopValueStatic(initialValue, minValue, maxValue);
+            Value = initialValue;
         }
-
-        public IDisposable Subscribe(Action<T, T> onLoop)
+        public LoopableInt SetMax(int maxValue)
+        {
+            _maxValue = maxValue;
+            if (Value > maxValue)
+                Value = Value;
+            return this;
+        }
+        public LoopableInt SetMin(int minValue)
+        {
+            _minValue = minValue;
+            if (Value < minValue)
+                Value = minValue;
+            return this;
+        }
+        public IDisposable Subscribe(Action<int, int> onLoop)
         {
             _onLoop += onLoop;
-            var ac = _onLoop;
-            return Disposable.Create(() => ac -= onLoop);
+            return Disposable.Create(() => _onLoop -= onLoop);
         }
 
-        public Loopable<T> SetMax(T maxValue)
+        public bool CanSetValue(int newValue)
         {
-            _maxValue = maxValue;
-            _value = LoopValue(_value);
-            return this;
+            throw new NotImplementedException();
         }
 
-        public Loopable<T> SetMin(T minValue)
-        {
-            _minValue = minValue;
-            _value = LoopValue(_value);
-            return this;
-        }
-
-        public bool CanSetValue(T newValue)
-        {
-            return Compare(newValue, _minValue) >= 0 && Compare(newValue, _maxValue) <= 0;
-        }
-
-        public override string ToString() => _value.ToString();
-        public static implicit operator T(Loopable<T> h) => h.Value;
-
-        // --- 内部ヘルパー ---
-
-        private T LoopValue(T value)
-        {
-            return LoopValueStatic(value, _minValue, _maxValue);
-        }
-
-        private static T LoopValueStatic(T value, T min, T max)
-        {
-            // dynamicで数値型の演算を汎用化
-            dynamic v = value, mn = min, mx = max;
-            dynamic range = (mx - mn) + 1;
-            if (range <= 0) return mn;
-
-            dynamic mod = (v - mn) % range;
-            if (mod < 0) mod += range;
-
-            return (T)(mn + mod);
-        }
-
-        private bool IsLooped(T oldValue, T newInput)
-        {
-            dynamic oldV = oldValue, newV = newInput, mn = _minValue, mx = _maxValue;
-            dynamic range = (mx - mn) + 1;
-            return Math.Abs(newV - oldV) >= range;
-        }
-
-        private static int Compare(T a, T b)
-        {
-            return a.CompareTo(b);
-        }
+        public static implicit operator int(LoopableInt loop) => loop.Value;
     }
 }
